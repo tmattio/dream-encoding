e05a80d1c76f7d7e1ac19559dc9c9b8e805b6ca1
Thibaut Mattio <thibaut.mattio@gmail.com>
2021-05-17 18:46:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Dream Encoding
+
+[![Actions Status](https://github.com/tmattio/dream-encoding/workflows/CI/badge.svg)](https://github.com/tmattio/dream-encoding/actions)
+
+Encoding primitives for Dream applications.
+
+## To Do
+
+- [ ] Add unit tests
+- [ ] Write README
---
+++ lib/dream_encoding.mli
@@ -0,0 +1,48 @@
+(** Encoding primitives for Dream.
+
+    As of now, the supported encoding directives are [deflate] and [gzip]. More
+    directive will be supported when their support land in [decompress], the
+    underlying compression library used by Dream_encoding. *)
+
+val compress : Dream.middleware
+(** Middleware that reads the [Accept-Encoding] header of the request and
+    compresses the responses with the preferred supported algorithm. *)
+
+val decompress : Dream.middleware
+(** Middleware that reads the [Content-Encoding] of the request and decompresses
+    the body if all of the directives of the header are supported.
+
+    If one or more of the directive is not supported, an HTTP response
+    [415 Unsupported Media Type] is returned to the client.
+
+    Note that although HTTP supports encoding requests, it is rarely used in
+    practice. See [compress] to for a middleware that compresses the responses
+    instead. *)
+
+val with_encoded_body
+  :  ?algorithm:[ `Deflate | `Gzip ]
+  -> string
+  -> Dream.response
+  -> Dream.response
+(** [with_encoded_body ?algorithm body response] replaces the body of the
+    response with [body] compressed with [algorithm].
+
+    [algorithm] defaults to [`Deflate]. *)
+
+val accept_encoding
+  :  Dream.request
+  -> [ `Deflate | `Gzip | `Unknown of string ] list option
+(** Retrieve the list of accepted encoding directives from the [Accept-Encoding]
+    header.
+
+    If the request does not have an [Accept-Encoding] header, this returns
+    [None]. *)
+
+val preferred_content_encoding : Dream.request -> [ `Deflate | `Gzip ] option
+(** Retrieve preferred encoding directive from the [Accept-Encoding].
+
+    The preferred encoding directive is the first supported algorithm in the
+    list of accepted directives.
+
+    If no algorithm is supported, or if the request does not have an
+    [Accept-Encoding] header, this returns [None]. *)
